438482abab162df25bdbf186c88189aba2bd9449
Adarsha Shrestha <adarshabac19@oic.edu.np>
2023-11-23 00:13:40 +0545

    db

--- db.js
+++ db.js
@@ -1,8 +1,8 @@
 const {Sequelize} = require('sequelize')
 
-database = process.env.DB_NAME;
-username = process.env.DB_USER;
-password = process.env.DB_PASS;
+database = process.env.DB_NAME,
+username = process.env.DB_USER,
+password = process.env.DB_PASS
 
 
 const db = new Sequelize({
--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+require('dotenv').config();
+
 const express = require('express');
 const router = require('./router');
 const bodyparser = require('body-parser');
